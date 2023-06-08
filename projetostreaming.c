f774de1e21bd4a2ce920398d8734eed79e8028a6
armassaro <132797747+armassaro@users.noreply.github.com>
2023-06-08 18:55:07 -0300

    Update projetostreaming.c

--- projetostreaming.c
+++ projetostreaming.c
@@ -9,112 +9,111 @@ int main() {
 
     getmaxyx(stdscr, yterminal, xterminal);
 
-    // mvprintw(yterminal / 2, xterminal / 2 - 4, "Carregando...\n");
-    // mvprintw((yterminal / 2) - 2, 2, "[");
-    // mvprintw((yterminal / 2) - 2, xterminal - 2, "]");
-    // refresh();
+    mvprintw(yterminal / 2, xterminal / 2 - 4, "Carregando...\n");
+    mvprintw((yterminal / 2) - 2, 2, "[");
+    mvprintw((yterminal / 2) - 2, xterminal - 2, "]");
+    refresh();
 
-    // for(int a = 4; a <= xterminal - 4; a++) {  //tela de carregamento
+    for(int a = 4; a <= xterminal - 4; a++) {  //tela de carregamento
 
-    //     mvprintw((yterminal / 2) - 2, a, ">");
+        mvprintw((yterminal / 2) - 2, a, ">");
         
-    //     refresh();
-    //     usleep(20000);
+        refresh();
+        usleep(20000);
 
-    //     mvprintw((yterminal / 2) - 2, a, "|");
+        mvprintw((yterminal / 2) - 2, a, "|");
 
-    //     refresh();
+        refresh();
 
-    //     usleep(5000);
+        usleep(5000);
 
-    // }
+    }
 
-    // for(int a = 4; a <= xterminal - 4; a++) {  //preenche risco com hashtag
+    for(int a = 4; a <= xterminal - 4; a++) {  //preenche risco com hashtag
 
-    //     mvprintw((yterminal / 2) - 2, a, "#");
+        mvprintw((yterminal / 2) - 2, a, "#");
         
-    //     refresh();
+        refresh();
 
-    //     usleep(6000);
+        usleep(6000);
 
-    // }
+    }
 
-    // beep();
+    clear();
 
-    // clear();
+    mvprintw((yterminal / 2) - 2, xterminal / 2, "#");
 
-    // mvprintw((yterminal / 2) - 2, xterminal / 2, "#");
+    for(int a = yterminal / 2; a <= yterminal - 4; a++) {  //move um asterisco centralizado pra baixo
 
-    // for(int a = yterminal / 2; a <= yterminal - 4; a++) {  //move um asterisco centralizado pra baixo
-
-    //     mvprintw(a, xterminal / 2, "#");
+        mvprintw(a, xterminal / 2, "#");
         
-    //     refresh();
+        refresh();
 
-    //     usleep(10000);
+        usleep(10000);
 
-    //     clear();
+        clear();
 
-    // }
+    }
 
-    // for(int a = xterminal / 2; a <= xterminal - 4; a++) {  //move o asterisco pra direita
+    for(int a = xterminal / 2; a <= xterminal - 4; a++) {  //move o asterisco pra direita
 
-    //     mvprintw(yterminal - 4, a, "#");
+        mvprintw(yterminal - 4, a, "#");
         
-    //     refresh();
+        refresh();
 
-    //     usleep(10000);
+        usleep(10000);
 
-    //     clear();
+        clear();
 
-    // }
+    }
 
-    // for(int a = yterminal - 4; a >= 4; a--) {  //sobe e preenche coluna direita
+    for(int a = yterminal - 4; a >= 4; a--) {  //sobe e preenche coluna direita
 
-    //     mvprintw(a, xterminal - 4, "#");
+        mvprintw(a, xterminal - 4, "#");
         
-    //     refresh();
+        refresh();
 
-    //     usleep(10000);
+        usleep(10000);
 
-    // }
+    }
 
-    // for(int a = xterminal - 4; a >= 4; a--) {  //vai pra esquerda e preenche linha de cima
+    for(int a = xterminal - 4; a >= 4; a--) {  //vai pra esquerda e preenche linha de cima
 
-    //     mvprintw(4, a, "-");
-    //     refresh();
+        mvprintw(4, a, "-");
+        refresh();
 
-    //     usleep(10000);
+        usleep(10000);
 
-    // }
+    }
 
-    // for(int a = 5; a <= yterminal - 4; a++) {  //preenche coluna esquerda
+    for(int a = 5; a <= yterminal - 4; a++) {  //preenche coluna esquerda
 
-    //     mvprintw(a, 4, "#");
+        mvprintw(a, 4, "#");
         
-    //     refresh();
+        refresh();
 
-    //     usleep(10000);
+        usleep(10000);
 
-    // }
+    }
 
-    // for(int a = 4; a <= xterminal - 4; a++) {  //preenche linha inferior
+    for(int a = 4; a <= xterminal - 4; a++) {  //preenche linha inferior
 
-    //     mvprintw(yterminal - 4, a, "-");
-    //     refresh();
+        mvprintw(yterminal - 4, a, "-");
+        refresh();
 
-    //     usleep(10000);
+        usleep(10000);
 
-    // }
+    }
 
     WINDOW *borda = newwin(yterminal - 7, xterminal - 7, 4, 4);
 
-    int xborda, yborda;
+    int xborda, yborda;  //variáveis utilizadas para armazenar as dimensões da borda da janela
 
     char *stringlogopt1 = "  __                                                             \n";
     char *stringlogopt2 = " (_ _|_ ._ _   _. ._ _  o ._   _    |\\/|  _. ._   _.  _   _  ._ \n";
     char *stringlogopt3 = " __) |_ | (/_ (_| | | | | | | (_|   |  | (_| | | (_| (_| (/_ |   \n";
     char *stringlogopt4 = "                               _|                     _|         \n";
+
 
     getmaxyx(borda, yborda, xborda);
 
@@ -136,10 +135,9 @@ int main() {
     int opcoes = 2;
     int opcao;
     int highlight = 0;
+
     keypad(menuopcoes, TRUE);
-
     werase(menuopcoes);
-
 
     while(1) {
 
@@ -195,7 +193,7 @@ int main() {
 
                     wclear(stdscr);
                     wclear(borda);
-                    MensagemBoasVindas(yterminal, xterminal, borda);
+                    MensagemBoasVindas(yterminal, xterminal, borda, yborda, xborda);
 
                 }
 
@@ -221,73 +219,93 @@ int main() {
 
 }
 
-void MensagemBoasVindas(int yterminal, int xterminal, WINDOW *borda) {
+void MensagemBoasVindas(int yterminal, int xterminal, WINDOW *borda, int yborda, int xborda) {
 
-    cbreak();
     refresh();
     wclear(borda);
     wborder(borda, '#', '#', '-', '-', '-', '-', '-', '-');  //printa de novo a borda da janela
     wrefresh(borda);  //atualiza a borda 
 
-    // char *mensagem1 = "Seja bem vindo ao Streaming Manager!";
-    // char *mensagem2 = "O Streaming Manager eh um projeto desenvolvido pelos alunos Arthur Massaro, Pedro Lorin e Matheus Conrado";
-    // char *mensagem3 = "Por meio deste programa, eh possivel gerenciar suas series favoritas!";
-    // char *mensagem4 = "Eh possivel cadastrar, alterar, remover e listar mais de 250 series dentre as plataformas mais populares como Netflix, Amazon Prime Video e HBO Max!";
-    // char *mensagem5 = "Parece que nao existe um arquivo contendo os dados das séries... Gostaria de criar um agora?";
+    char *mensagem1 = "Seja bem vindo ao Streaming Manager!";
+    char *mensagem2 = "O Streaming Manager eh um projeto desenvolvido pelos alunos Arthur Massaro, Pedro Lorin e Matheus Conrado";
+    char *mensagem3 = "Por meio deste programa, eh possivel gerenciar suas series favoritas!";
+    char *mensagem4 = "Eh possivel cadastrar, alterar, remover e listar mais de 250 series dentre as plataformas mais populares como Netflix, Amazon Prime Video e HBO Max!";
+    char *mensagem5 = "Parece que nao existe um arquivo contendo os dados das series... Gostaria de criar um agora?";
 
     char *opcoesNome[] = {"Sim", "Nao"};
 
-    // curs_set(TRUE);
+    curs_set(TRUE);
 
-    // for(int a = 0; a <= strlen(mensagem1) - 1; a++) {  //printa na tela a mensagem 1
+    int x;
 
-    //     mvaddch(6, a + 6, mensagem1[a]);
-    //     move(6, a + 7);
-    //     refresh();
-    //     usleep(50000);
+    x = (xborda - strlen(mensagem1)) / 2; //variável utilizada no cálculo da centralização da mensagem
 
-    // }
+    for(int a = 0; a <= strlen(mensagem1) - 1; a++) {  //printa na tela a mensagem 1
 
-    // for(int a = 0; a <= strlen(mensagem2) - 1; a++) {  //printa na tela a mensagem 2
+        mvwaddch(borda, yborda / 2 - 5, a + x, mensagem1[a]);
+        wmove(borda, yborda / 2 - 5, a + x + 1);
+        wrefresh(borda);
+        usleep(30000);
 
-    //     mvaddch(7, a + 6, mensagem2[a]);
-    //     move(7, a + 7);
-    //     refresh();
-    //     usleep(50000);
+    }
 
-    // }
+    x = (xborda - strlen(mensagem2)) / 2;
 
-    // for(int a = 0; a <= strlen(mensagem3) - 1; a++) {  //printa na tela a mensagem 3
+    for(int a = 0; a <= strlen(mensagem2) - 1; a++) {  //printa na tela a mensagem 2
 
-    //     mvaddch(8, a + 6, mensagem3[a]);
-    //     move(8, a + 7);
-    //     refresh();
-    //     usleep(50000);
+        mvwaddch(borda, yborda / 2 - 3, a + x, mensagem2[a]);
+        wmove(borda, yborda / 2 - 3, a + x + 1);
+        wrefresh(borda);
+        usleep(30000);
 
-    // }
+    }
 
-    // for(int a = 0; a <= strlen(mensagem4) - 1; a++) {  //printa na tela a mensagem 4
+    x = (xborda - strlen(mensagem3)) / 2;
 
-    //     mvaddch(10, a + 6, mensagem4[a]);
-    //     move(10, a + 7);
-    //     refresh();
-    //     usleep(50000);
+    for(int a = 0; a <= strlen(mensagem3) - 1; a++) {  //printa na tela a mensagem 3
 
-    // }
+        mvwaddch(borda, yborda / 2 - 2, a + x, mensagem3[a]);
+        wmove(borda, yborda / 2 - 2, a + x + 1);
+        wrefresh(borda);
+        usleep(30000);
+
+    }
+
+    x = (xborda - strlen(mensagem4)) / 2;
+
+    for(int a = 0; a <= strlen(mensagem4) - 1; a++) {  //printa na tela a mensagem 4
+
+        mvwaddch(borda, yborda / 2 - 1, a + x, mensagem4[a]);
+        wmove(borda, yborda / 2 - 1, a + x + 1);
+        wrefresh(borda);
+        usleep(30000);
+
+    }
+
+    x = (xborda - strlen(mensagem5)) / 2;
+
+    for(int a = 0; a <= strlen(mensagem5) - 1; a++) {  //printa na tela a mensagem 5
+
+        mvwaddch(borda, yborda / 2 + 1, a + x, mensagem5[a]);
+        wmove(borda, yborda / 2 + 1, a + x + 1);
+        wrefresh(borda);
+        usleep(30000);
+
+    }
 
     curs_set(FALSE);
 
-    WINDOW *AlternativaSimNao = newwin(2, xterminal, 12, 18);  //PRECISA SER AJUSTADO
+    WINDOW *AlternativaSimNao = newwin(1, 153, (yborda / 2) + 7, 8); 
 
     int opcoes = 2;
     int opcao;
     int highlight = 0;
-    keypad(AlternativaSimNao, TRUE);
+    keypad(AlternativaSimNao, TRUE);  //habilita as setas como entrada do teclado
     werase(AlternativaSimNao);
 
     while(1) {
 
-        for(int a = 0; a < opcoes; a++) {
+        for(int a = 0; a < opcoes; a++) {  //imprime sim e nao
 
             if(a == highlight) {
 
@@ -295,10 +313,18 @@ void MensagemBoasVindas(int yterminal, int xterminal, WINDOW *borda) {
 
             }
 
-            int x = (15 - strlen(opcoesNome[a])) / 2;
-            mvwprintw(AlternativaSimNao, 10, 8, "%s", opcoesNome[a]);
-            wattroff(AlternativaSimNao, A_REVERSE);
+            if(a == 0) {
+                
+                mvwprintw(AlternativaSimNao, 0, 42, "%s", opcoesNome[a]);
+                wattroff(AlternativaSimNao, A_REVERSE);
+            
+            }
+            else {
 
+                mvwprintw(AlternativaSimNao, 0, 108, "%s", opcoesNome[a]);
+                wattroff(AlternativaSimNao, A_REVERSE);
+
+            }
         }
 
         opcao = wgetch(AlternativaSimNao);
@@ -316,9 +342,9 @@ void MensagemBoasVindas(int yterminal, int xterminal, WINDOW *borda) {
 
             case KEY_LEFT:
             highlight--;
-            if(highlight >= opcoes) {
+            if(highlight < 0) {
 
-                highlight = opcoes - 1;
+                highlight = 0;
 
             }
             break;
@@ -327,7 +353,9 @@ void MensagemBoasVindas(int yterminal, int xterminal, WINDOW *borda) {
             
             if (highlight == 0) {
                 
+                wclear(AlternativaSimNao);
                 wprintw(AlternativaSimNao, "sucesso");
+                wrefresh(AlternativaSimNao);
                 getch();
                 endwin();
                 exit(1);
